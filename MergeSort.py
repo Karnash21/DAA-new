a64335ad1d19c7569bed78f25b2d0c80af5f1a8e
Karnash21 <ashutoshk2019@gmail.com>
2022-06-29 13:08:44 +0530

    Deleted extra comments

--- MergeSort.py
+++ MergeSort.py
@@ -1,38 +1,23 @@
-# Python program for implementation of MergeSort
-
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
-
+# Python program for time complexity graph of mergesort
 
 from random import randint
 from math import log2
 import timeit
 import matplotlib.pyplot as plt
 
-
-
-
-def merge(arr, l, m, r):
+def merge(arr, l, m, r):#merge function to merge the arrays
 	n1 = m - l + 1
 	n2 = r - m
-
-	# create temp arrays
 	L = [0] * (n1)
 	R = [0] * (n2)
 
-	# Copy data to temp arrays L[] and R[]
 	for i in range(0, n1):
 		L[i] = arr[l + i]
-
 	for j in range(0, n2):
 		R[j] = arr[m + 1 + j]
-
-	# Merge the temp arrays back into arr[l..r]
-	i = 0	 # Initial index of first subarray
-	j = 0	 # Initial index of second subarray
-	k = l	 # Initial index of merged subarray
-
+	i = 0
+	j = 0
+	k = l
 	while i < n1 and j < n2:
 		if L[i] <= R[j]:
 			arr[k] = L[i]
@@ -41,62 +26,34 @@ def merge(arr, l, m, r):
 			arr[k] = R[j]
 			j += 1
 		k += 1
-
-	# Copy the remaining elements of L[], if there
-	# are any
 	while i < n1:
 		arr[k] = L[i]
 		i += 1
 		k += 1
-
-	# Copy the remaining elements of R[], if there
-	# are any
 	while j < n2:
 		arr[k] = R[j]
 		j += 1
 		k += 1
-
-# l is for left index and r is right index of the
-# sub-array of arr to be sorted
-
-
-def mergeSort(arr, l, r):
+def mergeSort(arr, l, r):#mergesort function to perform mergesort
 	if l < r:
-
-		# Same as (l+r)//2, but avoids overflow for
-		# large l and h
 		m = l+(r-l)//2
-
-		# Sort first and second halves
 		mergeSort(arr, l, m)
 		mergeSort(arr, m+1, r)
 		merge(arr, l, m, r)
-
-
-# Driver code to test above
 arr = []
 x = []
 y = []
 n = []
-for i in range(1, 50000, 500):
-    # n = len(arr)
+for i in range(1, 5000, 50):
     for j in range(i):
         temp = randint(0, 100000)
         arr.append(temp)
-    # print("Given array is")
-    # for k in range(i):
-
     x.append(i)
-    start = timeit.default_timer()
+    start = timeit.default_timer()#starts timer
     mergeSort(arr, 0, i-1)
-    end = timeit.default_timer()
+    end = timeit.default_timer()#stops timer
     y.append(end - start)
     n.append(i*log2(i)*0.00000103)
-    # print(end - start)
-    # print("\n\nSorted array is")
-    # for l in range(i):
-    #     print("%d" % arr[l],end=" ")
-    # print("\n")
 plt.plot(x, y, label = 'MergeSort', color = 'blue')
 plt.plot(x, n, label = 'nlogn', color = 'red')
 plt.legend()
@@ -104,4 +61,3 @@ plt.xlabel('Array size ------>')
 plt.ylabel('Time taken (s) ------>')
 plt.title('Merge Sort')
 plt.show()
-
